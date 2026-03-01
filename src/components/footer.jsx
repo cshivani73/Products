68b2bea2c8377bcbfbf88201b3ff8b863069c5a4
shivani <shivanichouhan697@gmail.com>
2026-03-01 23:07:20 +0530

    updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="footer">
-        © 2026 Shivani Chouhan — Full-Stack Developer
+        ©2026 Shivani Chouhan — Full-Stack Developer 
       </footer>
     );
   }
